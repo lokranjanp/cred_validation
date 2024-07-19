abad9baff13e813dfbb24069db4c86b84689a1ad
lokranjanp <lokranjan03@gmail.com>
2024-07-19 18:11:35 +0530

    add basic logic

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+# app.py
+from flask import Flask, render_template, request, redirect, url_for, flash
+from luhn import luhn_check, generate_luhn_number
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/generate', methods=['GET', 'POST'])
+def generate():
+    if request.method == 'POST':
+        prefix = request.form['prefix']
+        length = int(request.form['length'])
+        number = generate_luhn_number(prefix, length)
+        return render_template('generate.html', number=number)
+    return render_template('generate.html')
+
+
+@app.route('/validate', methods=['GET', 'POST'])
+def validate():
+    if request.method == 'POST':
+        number = request.form['number']
+        is_valid = luhn_check(number)
+        return render_template('validate.html', number=number, is_valid=is_valid)
+    return render_template('validate.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ luhn.py
@@ -0,0 +1,36 @@
+# luhn.py
+def digits_of(n):
+    return [int(d) for d in str(n)]
+
+
+def luhn_check(num):
+    digits = digits_of(num)
+    checksum = 0
+    is_second = False
+    for d in reversed(digits):
+        if is_second:
+            d = d * 2
+            if d > 9:
+                d = d - 9
+        checksum += d
+        is_second = not is_second
+    return checksum % 10 == 0
+
+
+def generate_luhn_number(prefix, length):
+    import random
+    num = [int(x) for x in str(prefix)]
+    while len(num) < (length - 1):
+        num.append(random.randint(0, 9))
+    checksum = 0
+    is_second = True
+    for d in reversed(num):
+        if is_second:
+            d = d * 2
+            if d > 9:
+                d = d - 9
+        checksum += d
+        is_second = not is_second
+    check_digit = (10 - (checksum % 10)) % 10
+    num.append(check_digit)
+    return ''.join(map(str, num))
